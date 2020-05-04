1adb1816bef949f8fdd1710217ede8cae279fdc2
Izabela <55464259+IzabelaSpirovska@users.noreply.github.com>
2020-05-04 22:59:15 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import sqlite3
+
+app = FastAPI()
+
+
+@app.on_event('startup')
+async def startup():
+    app.database = sqlite3.connect('chinook.db')
+
+@app.on_event('shutdown')
+async def shutdown():
+    app.database.close() 
+
+
+@app.get('/path')
+async def list_of_objects(int = 0, per_page: int = 10):
+	app.database.row_factory = sqlite3.Row 
+	path = app.database.execute[
+        {
+            "TrackId": int,
+            "Name": str,
+            "AlbumId": int,
+            "MediaTypeId": int,
+            "GenreId": int,
+            "Composer": str,
+            "Milliseconds": int,
+            "Bytes": int,
+            "UnitPrice": float 
+	}
+        ].fetchall() 
+	return path
