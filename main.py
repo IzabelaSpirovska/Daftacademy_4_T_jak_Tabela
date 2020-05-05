8ad7847eb4e0a3a0da9b2a32a954258658a67758
Izabela <55464259+IzabelaSpirovska@users.noreply.github.com>
2020-05-05 21:33:13 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, Depends
+import sqlite3
+
+app = FastAPI()
+
+
+@app.on_event('startup')
+async def startup():
+    app.database = sqlite3.connect('chinook.db')
+
+@app.on_event('shutdown')
+async def shutdown():
+    app.database.close() 
+
+
+@app.get('/tracks')
+async def list_of_objects(page: int = 0, per_page: int = 10):
+	app.database.row_factory = sqlite3.Row 
+	tracks = app.database.execute(
+		f"SELECT * FROM tracks LIMIT {per_page} OFFSET {page*per_page}" 
+		).fetchall() 
+	return tracks
