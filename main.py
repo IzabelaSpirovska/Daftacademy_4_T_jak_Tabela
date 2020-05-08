910b737ccad6817e0f9695dc2d9799c12e9feb8e
Izabela <55464259+IzabelaSpirovska@users.noreply.github.com>
2020-05-08 16:06:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
+from fastapi import FastAPI, HTTPException, Response, status
+from pydantic import BaseModel
 import sqlite3
-from fastapi import FastAPI, HTTPException
-
-
 app = FastAPI()
 
-
-@app.on_event('startup')
+@app.on_event("startup")
 async def startup():
     app.db_connection = sqlite3.connect('chinook.db')
+    app.db_connection.row_factory = sqlite3.Row
 
-@app.on_event('shutdown')
+
+@app.on_event("shutdown")
 async def shutdown():
-    app.db_connection.close() 
+    app.db_connection.close()
 
 
-@app.get('/tracks')
-async def list_of_objects(page: int = 0, per_page: int = 10):
-	app.db_connection.row_factory = sqlite3.Row
-	tracks = app.db_connection.execute(
-		'SELECT * FROM tracks ORDER BY TrackId').fetchall()
-	current_tracks = data[per_page * page:per_page * (page+1)] 
-	return current_tracks
+@app.get("/tracks")
+async def tracks(page: int = 0, per_page:int = 10):
+    cursor = app.db_connection.cursor()
+    data = cursor.execute("SELECT * FROM tracks ORDER BY TrackId LIMIT :per_page OFFSET :per_page*:page",
+        {'page': page, 'per_page': per_page}).fetchall()
+    return data
