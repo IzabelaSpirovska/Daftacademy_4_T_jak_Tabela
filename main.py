37d932f03ef8c5213d35114d4828968114be6cc6
Izabela <55464259+IzabelaSpirovska@users.noreply.github.com>
2020-05-08 15:43:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
-from fastapi import FastAPI, Depends
 import sqlite3
+from fastapi import FastAPI, HTTPException
+
 
 app = FastAPI()
 
 
 @app.on_event('startup')
 async def startup():
-    app.database = sqlite3.connect('chinook.db')
+    app.db_connection = sqlite3.connect('chinook.db')
 
 @app.on_event('shutdown')
 async def shutdown():
-    app.database.close() 
+    app.db_connection.close() 
 
 
 @app.get('/tracks')
 async def list_of_objects(page: int = 0, per_page: int = 10):
-	app.database.row_factory = sqlite3.Row 
-	tracks = app.database.execute(
-		f"SELECT * FROM tracks LIMIT {per_page} OFFSET {page*per_page}" 
-		).fetchall() 
-	return tracks
+	app.db_connection.row_factory = sqlite3.Row
+	tracks = app.db_connection.execute(
+		'SELECT * FROM tracks ORDER BY TrackId').fetchall()
+	current_tracks = data[per_page * page:per_page * (page+1)] 
+	return current_tracks
